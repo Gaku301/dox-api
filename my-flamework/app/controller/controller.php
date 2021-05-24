a54f6359ddb04e3fd390530669e7dc6c8b90e752
Gaku Ishikawa <gaku.jp.301@gmail.com>
2021-05-25 08:13:01 +0900

    composer

---
+++ my-flamework/app/controller/controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controller;
+
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
+require_once dirname(__FILE__).'/../utility/utility.php';
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+/*
+ * ベースとなるコントローラー
+ * (共通処理等を記載する)
+ */
+class controller
+{
+    private $name = 'controller';
+
+    protected function __construct()
+    {
+        // コントローラーを直接呼ばれてもnewできないように
+    }
+
+    // ログ出力
+    public function logging($message, string $file_name = 'app.log')
+    {
+        $Logger = new Logger('logger');
+        $Logger->pushHandler(new StreamHandler(__DIR__.'/../../logs/'.$file_name, Logger::INFO));
+        $Logger->addInfo($message);
+    }
+}
--- my-flamework/public/index.php
+++ my-flamework/public/index.php
@@ -1,2 +1,39 @@
 <?php
-phpinfo();
+/**
+ * 全てのリクエストは必ずこのファイルを経由する.
+ */
+if (empty($_SERVER['REQUEST_URI'])) {
+    exit;
+}
+
+// URLをスラッシュで分解
+$parse_uri_array = explode('/', $_SERVER['REQUEST_URI']);
+// 最後の文字を取り出す
+$last_uri = end($parse_uri_array);
+// クエリ文字列を外す
+$called = substr($last_uri, 0, strcspn($last_uri, '?'));
+
+// app/controller/配下に同名のPHPファイルがないか探す
+if (file_exists('../app/controller/'.$called.'.php')) {
+    // 該当ファイルをインクルードし、コントローラーをインスタンス化する
+    include '../app/controller/'.$called.'.php';
+    $class = 'app\contoller\\'.$called;
+    $class_obj = new $class();
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // POSTであれば、コントローラーのpostメソッドを呼び出す
+        $response = $class_obj->post();
+    } else {
+        // それ以外は空で返す
+        $response = [];
+    }
+
+    // Origin null is not allowed by Access-Control-Allow-Origin.とかのエラー回避の為、ヘッダー付与
+    header('Access-Control-Allow-Ofigin: *');
+    // json形式で値を返す
+    echo json_encode($response);
+} else {
+    // 該当ファイルがなければエラーを返す
+    header('HTTP/1.0 404 Not Found');
+    exit;
+}
